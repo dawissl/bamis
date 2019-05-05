890eaba970d57da5a17768a41e1a37cb9ab6c8e3
Igor Milenkovic <milenkovicig@gmail.com>
2019-05-05 23:00:40 +0200

    formulare na pridavani

--- src/main/java/cz/uhk/dbs2/bamis/controller/CustomerController.java
+++ src/main/java/cz/uhk/dbs2/bamis/controller/CustomerController.java
@@ -1,12 +1,15 @@
 package cz.uhk.dbs2.bamis.controller;
 
 
+import cz.uhk.dbs2.bamis.model.Customer;
 import cz.uhk.dbs2.bamis.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author David Sladecek
@@ -24,6 +27,13 @@ public class CustomerController {
     @GetMapping(value = "/customer")
     public String packages(Model model) {
         model.addAttribute("customers",customerService.loadAllCustomers());
+        return "customer";
+    }
+
+    @PostMapping("/customer")
+    public String addCustomer(Model model, @ModelAttribute(value = "customer") Customer customer) {
+        customerService.addCustomer(customer);
+        model.addAttribute("customers", customerService.loadAllCustomers());
         return "customer";
     }
 }
--- src/main/java/cz/uhk/dbs2/bamis/controller/EmployeeController.java
+++ src/main/java/cz/uhk/dbs2/bamis/controller/EmployeeController.java
@@ -1,10 +1,14 @@
 package cz.uhk.dbs2.bamis.controller;
 
+import cz.uhk.dbs2.bamis.model.Employee;
+import cz.uhk.dbs2.bamis.service.EmployeeCategoryService;
 import cz.uhk.dbs2.bamis.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * @author David Sladecek
@@ -13,15 +17,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EmployeeController {
 
     EmployeeService employeeService;
+    EmployeeCategoryService employeeCategoryService;
 
     @Autowired
-    public EmployeeController(EmployeeService employeeService) {
+    public EmployeeController(EmployeeService employeeService,
+                              EmployeeCategoryService employeeCategoryService) {
         this.employeeService = employeeService;
+        this.employeeCategoryService = employeeCategoryService;
     }
 
     @GetMapping(value = "/employee")
     public String employees(Model model) {
         model.addAttribute("employees",employeeService.loadAllEmployees());
+        model.addAttribute("kategorie",employeeCategoryService.loadAllEmployeeCategories());
+        return "employee";
+    }
+
+    @PostMapping("/employee")
+    public String addPackage(Model model, @ModelAttribute(value = "employee") Employee employee) {
+        employeeService.addEmployee(employee);
+        model.addAttribute("employees", employeeService.loadAllEmployees());
         return "employee";
     }
 }
